497cc1f34a9cf4d9a8fd616b60360841354d0626
bieniajl <j.bienias@web.de>
2018-04-08 10:43:25 +0200

    Make database work

--- setup.js
+++ setup.js
@@ -1,9 +1,9 @@
 
 const mysql = require('mysql2');
 var config = {
-    host: 'icon-db.mysql.database.azure.com',
-    user: 'wolf@icon-db',
-    password: 'EJ6chESAmK',
+    host: 'ww-data-host.mysql.database.azure.com',
+    user: 'database@ww-data-host',
+    password: 'uJHeCu3P!',
     port: 3306,
     ssl: true
 };
@@ -17,28 +17,64 @@ conn.connect(
         }
         else {
             console.log("Connection established.");
-            var sql = "DROP DATABASE projectDb;";
+            var sql = "DROP DATABASE IF EXISTS projectDb;";
             conn.query(sql, function (err, results, fields) {
                 if (err) {
                     throw err;
                 }
             });
-            sql = [
-                "CREATE Database projectDb;",
-                "USE projectDb CREATE TABLE `Project` (`id` varchar(50) NOT NULL,`creatorId` varchar(50) NOT NULL,`iconId` varchar(50) NOT NULL`name` varchar(50) NOT NULL`description` TEXT NOT NULL,`creationDate` DATE NOT NULL,PRIMARY KEY (`id`));",
-                "USE projectDb CREATE TABLE `Milestone` (`id` varchar(50) NOT NULL,`projectiId` varchar(50) NOT NULL,PRIMARY KEY (`id`));",
-                "USE projectDb CREATE TABLE `IssueMilestone` (`issueId` varchar(50) NOT NULL,`milestoneId` varchar(50) NOT NULL,PRIMARY KEY (`issueId`,`milestoneId`));",
-                "ALTER TABLE `Milestone` ADD CONSTRAINT `Milestone_fk0` FOREIGN KEY (`projectId`) REFERENCES `Project`(`id`);",
-                "ALTER TABLE `IssueMilestone` ADD CONSTRAINT `IssueMilestone_fk0` FOREIGN KEY (`milestoneId`) REFERENCES `Milestone`(`id`);",
-            ];
+            sql = "CREATE Database projectDb;";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
 
-            for (var i in sql) {
-                conn.query(i, function (err, results, fields) {
-                    if (err) {
-                        throw err;
-                    }
-                });
-            }
+            sql = "USE projectDb;";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `Project` (`id` varchar(50) NOT NULL,`creatorId` varchar(50) NOT NULL,`iconId` varchar(50) NOT NULL,`name` varchar(50) NOT NULL,`description` TEXT NOT NULL,`creationDate` DATE NOT NULL,PRIMARY KEY (`id`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `Milestone` (`id` varchar(50) NOT NULL,`projectId` varchar(50) NOT NULL,PRIMARY KEY (`id`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `IssueMilestone` (`issueId` varchar(50) NOT NULL,`milestoneId` varchar(50) NOT NULL,PRIMARY KEY (`issueId`,`milestoneId`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "ALTER TABLE `Milestone` ADD CONSTRAINT `Milestone_fk0` FOREIGN KEY (`projectId`) REFERENCES `Project`(`id`);";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "ALTER TABLE `IssueMilestone` ADD CONSTRAINT `IssueMilestone_fk0` FOREIGN KEY (`milestoneId`) REFERENCES `Milestone`(`id`);";
+
+
+
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
 
         }
     });
